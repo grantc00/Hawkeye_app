d135d1040e0e0ac137210d06fe529f8f3cc13e82
grantc00 <grantcky@gmail.com>
2021-10-19 17:46:17 -0700

    user table done

--- python-project-starter-main/app/models/user.py
+++ python-project-starter-main/app/models/user.py
@@ -1,15 +1,20 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    first_name = db.Column(db.String(255), nullable=False)
+    last_name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
+    buying_power = db.Column(db.Integer, nullable=False)
     hashed_password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime(), default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime(), default=datetime.utcnow)
 
     @property
     def password(self):
@@ -25,6 +30,10 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
-            'username': self.username,
-            'email': self.email
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email': self.email,
+            'buying_power': self.buying_power,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
         }
